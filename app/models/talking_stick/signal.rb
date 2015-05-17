55279facd602322a8d666b8e95c1f8e9c701a6f1
Ben Klang <bklang@mojolingo.com>
2015-05-16 20:46:19 -0400

    Validations to ensure we have complete signals

--- app/models/talking_stick/signal.rb
+++ app/models/talking_stick/signal.rb
@@ -3,6 +3,7 @@ module TalkingStick
     belongs_to :room
     belongs_to :sender, class_name: "TalkingStick::Participant"
     belongs_to :recipient, class_name: "TalkingStick::Participant"
+    validates :room, :sender, :recipient, presence: true
 
     # The normal delegate method seems to not be working for an unknown reason
     def sender_guid
