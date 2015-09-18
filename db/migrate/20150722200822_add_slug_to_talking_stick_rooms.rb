f6a7e198bac7041b442c7a5a4b0778c79d67b9e5
Ben Klang <bklang@mojolingo.com>
2015-09-18 08:11:46 -0400

    Migration for existing rooms

--- db/migrate/20150722200822_add_slug_to_talking_stick_rooms.rb
+++ db/migrate/20150722200822_add_slug_to_talking_stick_rooms.rb
@@ -1,5 +1,11 @@
 class AddSlugToTalkingStickRooms < ActiveRecord::Migration
-  def change
+  def up
     add_column :talking_stick_rooms, :slug, :string
+    # Add slugs to the existing rooms
+    TalkingStick::Room.all.map &:save
+  end
+
+  def down
+    remove_column :talking_stick_rooms, :slug
   end
 end
