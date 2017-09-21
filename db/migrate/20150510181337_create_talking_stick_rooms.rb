de40db2cdd8a40cb29dec655138d1bd138902276
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2017-09-21 16:08:07 -0400

    Use ActiveRecord::Migration[4.2]
    This is compatible with Rails 4.2, 5.0 and 5.1

--- db/migrate/20150510181337_create_talking_stick_rooms.rb
+++ db/migrate/20150510181337_create_talking_stick_rooms.rb
@@ -1,4 +1,4 @@
-class CreateTalkingStickRooms < ActiveRecord::Migration[5.1]
+class CreateTalkingStickRooms < ActiveRecord::Migration[4.2]
   def change
     create_table :talking_stick_rooms do |t|
       t.string :name
