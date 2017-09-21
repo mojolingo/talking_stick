4bee946086cf1ba635265bf280175da01dc26ffd
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2017-09-21 16:11:14 -0400

    Fix problem with jquery-rails
    This is necessary to make sure jquery can be required like this:
    
        //= require jquery

--- lib/talking_stick/engine.rb
+++ lib/talking_stick/engine.rb
@@ -1,3 +1,5 @@
+require "jquery-rails"
+
 module TalkingStick
   class Engine < ::Rails::Engine
     engine_name "talking_stick"
@@ -5,8 +7,12 @@ module TalkingStick
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-      g.assets false
+      g.assets true
       g.helper false
+    end
+
+    initializer "talking_stick.assets.precompile" do |app|
+      app.config.assets.precompile += %w( application.js application.css )
     end
   end
 end
